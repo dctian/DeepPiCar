f5f89b057d175f6a54c06e5b1cc6927effc07340
David Tian <dctian@hotmail.com>
2019-04-20 11:27:09 -0500

    Add opencv test to check if installation went fine.

---
+++ driver/code/opencv_test.py
@@ -0,0 +1,22 @@
+import cv2
+
+def main():
+    camera = cv2.VideoCapture(-1)
+    camera.set(3, 640)
+    camera.set(4, 480)
+
+    while( camera.isOpened()):
+        _, image = camera.read()        
+        cv2.imshow('Original', image)
+        
+        b_w_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('B/W', b_w_image)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q') :
+            self.cleanup()
+            break
+            
+    cv2.destroyAllWindows()
+    
+if __name__ == '__main__':
+    main()
